de73695255dabc5768cdb77bd6f973b1e53cde53
Laknonglak <donovanlak@gmail.com>
2024-03-13 22:43:52 +0000

    first commit

---
+++ Example3/helper-function.js
@@ -0,0 +1,21 @@
+// Helper function to check if a number is positive
+  function isPositive(number) {
+    return number > 0;
+  }
+
+  // Function that uses the helper function to check if two numbers are positive
+  function checkNumbers(a, b, callPositive) {
+    if (callPositive(a) && isPositive(b)) {
+      console.log(`${a} and ${b} are both positive.`);
+    } else {
+      console.log(`${a} and/or ${b} are not both positive.`);
+    }
+  }
+
+  const num1 = 7;
+  const num2 = -3;
+
+  checkNumbers(num1, num2, isPositive);
+
+
+ 
